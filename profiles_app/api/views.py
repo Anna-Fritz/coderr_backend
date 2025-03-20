51cda06662a05910e34f4f450d600c211bc3f857
Anna Fritz <anna_eida@gmx.de>
2025-03-20 23:39:06 +0100

    feat(view): create views for user profiles

--- profiles_app/api/views.py
+++ profiles_app/api/views.py
@@ -1,13 +1,32 @@
 from rest_framework import generics
+from ..models import BusinessProfile, CustomerProfile
+from .serializers import CustomerProfileSerializer, BusinessProfileSerializer
 
 
 class ProfileDetailView(generics.RetrieveUpdateDestroyAPIView):
-    pass
+    queryset = BusinessProfile.objects.all()
+    lookup_field = 'pk'
+
+    def get_object(self):
+
+        try:
+            return BusinessProfile.objects.get(pk=self.kwargs['pk'])
+        except BusinessProfile.DoesNotExist:
+            return CustomerProfile.objects.get(pk=self.kwargs['pk'])
+
+    def get_serializer_class(self):
+        profile = self.get_object()
+
+        if profile.type == 'business':
+            return BusinessProfileSerializer
+        return CustomerProfileSerializer
 
 
 class BusinessProfileListView(generics.ListAPIView):
-    pass
+    queryset = BusinessProfile.objects.select_related('user').all()
+    serializer_class = BusinessProfileSerializer
 
 
 class CustomerProfileListView(generics.ListAPIView):
-    pass
+    queryset = CustomerProfile.objects.select_related('user').all()
+    serializer_class = CustomerProfileSerializer
