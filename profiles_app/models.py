af2339c54dfc131c194f2cbea6a5c838c96d70b6
Anna Fritz <anna_eida@gmx.de>
2025-03-20 23:37:51 +0100

    feat(models): optimize Profile Models

--- profiles_app/models.py
+++ profiles_app/models.py
@@ -9,9 +9,7 @@ from .api.utils import validate_file_size
 
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    first_name = models.CharField(max_length=15)
-    last_name = models.CharField(max_length=25)
-    file = models.FileField(upload_to='uploads/', validators=[FileExtensionValidator(allowed_extensions=['jpg', 'png']), validate_file_size])
+    file = models.FileField(upload_to='uploads/', validators=[FileExtensionValidator(allowed_extensions=['jpg', 'png']), validate_file_size], blank=True, null=True)
     uploaded_at = models.DateTimeField(auto_now_add=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
@@ -25,15 +23,13 @@ class BusinessProfile(UserProfile):
     description = models.TextField(max_length=250)
     working_hours = models.CharField(max_length=15)
     type = models.CharField(max_length=25, default="business", editable=False)
-    email = models.EmailField(max_length=50)
 
     def __str__(self):
-        return f"{self.first_name} {self.last_name} ({self.type})"
+        return f"{self.user.first_name} {self.user.last_name} ({self.type})"
 
 
 class CustomerProfile(UserProfile):
     type = models.CharField(max_length=25, default="customer", editable=False)
 
     def __str__(self):
-        return f"{self.first_name} {self.last_name} ({self.type})"
-
+        return f"{self.user.first_name} {self.user.last_name} ({self.type})"
