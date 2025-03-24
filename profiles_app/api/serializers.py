5f9b210ad81377a3b0c801ca094b11411b4cb259
Anna Fritz <anna_eida@gmx.de>
2025-03-24 12:56:22 +0100

    feat(serializer): optimize BusinessProfileSerializer and CustomerProfileSerializer

--- profiles_app/api/serializers.py
+++ profiles_app/api/serializers.py
@@ -1,73 +1,64 @@
 from rest_framework import serializers
-from ..models import CustomerProfile, BusinessProfile
-
-
-class CustomerProfileSerializer(serializers.ModelSerializer):
-    username = serializers.CharField(source='user.username', read_only=True)
-    first_name = serializers.CharField(source='user.first_name', required=False)
-    last_name = serializers.CharField(source='user.last_name', required=False)
-    email = serializers.CharField(source='user.email')
-    type = serializers.CharField(source='user.type')
-
-    class Meta:
-        model = CustomerProfile
-        fields = ['user', 'username', 'first_name', 'last_name', 'file', 'uploaded_at', 'type']
-        extra_kwargs = {
-            'file': {'required': False},
-            'uploaded_at': {'required': False},
-        }
-
-    def update(self, instance, validated_data):
-        user_data = validated_data.pop('user', {})
-
-        if 'first_name' in user_data:
-            instance.user.first_name = user_data['first_name']
-        if 'last_name' in user_data:
-            instance.user.last_name = user_data['last_name']
-        if 'email' in user_data:
-            instance.user.email = user_data['email']
-
-        instance.user.save()
-
-        return super().update(instance, validated_data)
-
-    def validate_file(self, value):
-        if value.size > 5 * 1024 * 1024:
-            raise serializers.ValidationError({'detail': '"Datei ist zu groß. Maximal 5 MB erlaubt.'})
-        return value
+from ..models import UserProfile
 
 
 class BusinessProfileSerializer(serializers.ModelSerializer):
-    username = serializers.CharField(source='user.username', read_only=True)
-    first_name = serializers.CharField(source='user.first_name', required=False)
-    last_name = serializers.CharField(source='user.last_name', required=False)
-    email = serializers.CharField(source='user.email')
-    type = serializers.CharField(source='user.type')
 
     class Meta:
-        model = BusinessProfile
-        fields = ['user', 'username', 'first_name', 'last_name', 'file', 'location', 'tel', 'description', 'working_hours', 'type']
+        model = UserProfile
+        fields = ['user', 'username', 'first_name', 'last_name', 'file', 'uploaded_at', 'created_at', 'type', 'location', 'tel', 'description', 'working_hours', 'email']
         extra_kwargs = {
             'file': {'required': False},
             'uploaded_at': {'required': False},
         }
 
     def update(self, instance, validated_data):
-        user_data = validated_data.pop('user', {})
+        user_data = validated_data.pop('user', None)
 
-        if 'first_name' in user_data:
-            instance.user.first_name = user_data['first_name']
-        if 'last_name' in user_data:
-            instance.user.last_name = user_data['last_name']
-        if 'email' in user_data:
-            instance.user.email = user_data['email']
+        instance.username = validated_data.get('username', instance.username)
+        instance.first_name = validated_data.get('first_name', instance.first_name)
+        instance.last_name = validated_data.get('last_name', instance.last_name)
+        instance.email = validated_data.get('email', instance.email)
+        instance.location = validated_data.get('location', instance.location)
+        instance.tel = validated_data.get('tel', instance.tel)
+        instance.description = validated_data.get('description', instance.description)
+        instance.working_hours = validated_data.get('working_hours', instance.working_hours)
+        instance.save()
 
-        instance.user.save()
+        if user_data:
+            user = instance.user
+            user.first_name = user_data.get('first_name', user.first_name)
+            user.last_name = user_data.get('last_name', user.last_name)
+            user.email = user_data.get('email', user.email)
+            user.save()
 
-        return super().update(instance, validated_data)
+        return instance
 
-    def validate_file(self, value):
-        if value.size > 5 * 1024 * 1024:
-            raise serializers.ValidationError({'detail': '"Datei ist zu groß. Maximal 5 MB erlaubt.'})
-        return value
 
+class CustomerProfileSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = UserProfile
+        fields = ['user', 'username', 'first_name', 'last_name', 'file', 'uploaded_at', 'type', 'email', 'created_at']
+        extra_kwargs = {
+            'file': {'required': False},
+            'uploaded_at': {'required': False},
+        }
+
+    def update(self, instance, validated_data):
+        user_data = validated_data.pop('user', None)
+
+        instance.username = validated_data.get('username', instance.username)
+        instance.first_name = validated_data.get('first_name', instance.first_name)
+        instance.last_name = validated_data.get('last_name', instance.last_name)
+        instance.email = validated_data.get('email', instance.email)
+        instance.save()
+
+        if user_data:
+            user = instance.user
+            user.first_name = user_data.get('first_name', user.first_name)
+            user.last_name = user_data.get('last_name', user.last_name)
+            user.email = user_data.get('email', user.email)
+            user.save()
+
+        return instance
