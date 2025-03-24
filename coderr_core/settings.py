257649e3fbf7372690fb98fdc894e65822d5a65b
Anna Fritz <anna_eida@gmx.de>
2025-03-24 13:00:47 +0100

    feat: add settings for file upload

--- coderr_core/settings.py
+++ coderr_core/settings.py
@@ -11,9 +11,13 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
 
 
 # Quick-start development settings - unsuitable for production
@@ -22,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-z+!)aep=(lqyhxobv2q4xx0$7xn(-ctupxsp_uuyov^^4f^vz9'
 
-AUTH_USER_MODEL = 'profiles_app.CustomUser'
+AUTH_USER_MODEL = 'user_auth_app.CustomUser'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
