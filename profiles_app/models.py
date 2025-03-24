3f0f6cb04692a6ea9908941402bcfb751bd5f926
Anna Fritz <anna_eida@gmx.de>
2025-03-24 12:55:18 +0100

    feat(model): optimize UserProfile and register in admin panel

--- profiles_app/models.py
+++ profiles_app/models.py
@@ -1,47 +1,40 @@
 from django.db import models
-from django.contrib.auth.models import User, AbstractUser, Group, Permission
 from django.core.validators import FileExtensionValidator
 from .api.utils import validate_file_size
+from django.utils import timezone
+from user_auth_app.models import CustomUser
 
 
 # Create your models here.
-class CustomUser(AbstractUser):
-    type = models.CharField(max_length=20, choices=[('customer', 'Customer'), ('business', 'Business')])
-
-    # Verwende related_name für die Reverse-Beziehungen
-    groups = models.ManyToManyField(
-        Group,
-        related_name='customuser_set',  # Einzigartiger Name für die Rückbeziehung
-        blank=True
-    )
-    user_permissions = models.ManyToManyField(
-        Permission,
-        related_name='customuser_permissions',  # Einzigartiger Name für die Rückbeziehung
-        blank=True
-    )
-
 
 class UserProfile(models.Model):
     user = models.OneToOneField(CustomUser, on_delete=models.CASCADE)
+    username = models.CharField(max_length=150, blank=True, null=True)
+    first_name = models.CharField(max_length=15, blank=True, null=True)
+    last_name = models.CharField(max_length=25, blank=True, null=True)
+    email = models.EmailField(max_length=50, blank=True, null=True)
     file = models.FileField(upload_to='uploads/', validators=[FileExtensionValidator(allowed_extensions=['jpg', 'png']), validate_file_size], blank=True, null=True)
-    uploaded_at = models.DateTimeField(auto_now_add=True)
-    created_at = models.DateTimeField(auto_now_add=True)
+    uploaded_at = models.DateTimeField(blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True, editable=False)
+    type = models.CharField(max_length=20, choices=[('customer', 'Customer'), ('business', 'Business')], editable=False)
+    location = models.CharField(max_length=30, blank=True, null=True)
+    tel = models.CharField(max_length=25, blank=True, null=True)
+    description = models.TextField(max_length=250, blank=True, null=True)
+    working_hours = models.CharField(max_length=15, blank=True, null=True)
 
-    class Meta:
-        abstract = True
-
-
-class BusinessProfile(UserProfile):
-    location = models.CharField(max_length=30)
-    tel = models.CharField(max_length=25)
-    description = models.TextField(max_length=250)
-    working_hours = models.CharField(max_length=15)
+    def save(self, *args, **kwargs):
+        if self.user:
+            if not self.first_name:
+                self.first_name = self.user.first_name
+            if not self.last_name:
+                self.last_name = self.user.last_name
+            if not self.email:
+                self.email = self.user.email
+            if not self.username:
+                self.username = self.user.username
+        if self.file and not self.uploaded_at:
+            self.uploaded_at = timezone.now()
+        super(UserProfile, self).save(*args, **kwargs)
 
     def __str__(self):
-        return f"{self.user.first_name} {self.user.last_name} ({self.type})"
-
-
-class CustomerProfile(UserProfile):
-
-    def __str__(self):
-        return f"{self.user.first_name} {self.user.last_name} ({self.type})"
+        return f"{self.username}, {self.first_name} {self.last_name} ({self.type})"
