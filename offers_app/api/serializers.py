14483db07dcba2572c83d9fa5243cc52006ea88a
Anna Fritz <anna_eida@gmx.de>
2025-03-30 01:38:21 +0100

    feat(serializer): optimize serializers for offer and offerdetails

--- offers_app/api/serializers.py
+++ offers_app/api/serializers.py
@@ -1,14 +1,58 @@
 from rest_framework import serializers
 from ..models import Offer, OfferDetail
+from rest_framework.reverse import reverse
+from django.db.models import Min
 
 
-class OfferSerializer(serializers.ModelSerializer):
-    user_details = serializers.SerializerMethodField()
-    details = serializers.HyperlinkedRelatedField(many=True, read_only=True, view_name="offerdetails-detail")
+class OfferDetailSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = OfferDetail
+        fields = ['id', 'title', 'revisions', 'delivery_time_in_days', 'price', 'features', 'offer_type']
+
+
+class OfferSerializer(serializers.ModelSerializer):   # POST / PUT
+    # user_details = serializers.SerializerMethodField()
+    details = OfferDetailSerializer(many=True)
 
     class Meta:
         model = Offer
-        fields = ['id', 'user', 'title', 'image', 'description', 'created_at', 'updated_at', 'details', 'user_details']
+        fields = ['id', 'title', 'image', 'description', 'details']
+
+    def create(self, validated_data):
+        user = self.context['request'].user
+        validated_data['user'] = user
+
+        details_data = validated_data.pop('details', None)  # Holt die Details-Daten
+        offer = Offer.objects.create(**validated_data)  # Erstellt das Angebot
+        for detail in details_data:
+            # Erstellt für jedes Detail einen OfferDetail-Eintrag
+            OfferDetail.objects.create(offer=offer, **detail)
+        return offer
+
+        # return super().create(validated_data)
+
+    def update(self, instance, validated_data):
+        details_data = validated_data.pop('details')  # Holt die Details-Daten
+        instance = super().update(instance, validated_data)
+
+        # Löscht alte OfferDetail und erstellt sie erneut
+        instance.details.all().delete()
+        for detail in details_data:
+            OfferDetail.objects.create(offer=instance, **detail)
+
+        return instance
+
+
+class OfferListSerializer(serializers.ModelSerializer):  # GET List
+    details = serializers.SerializerMethodField()
+    min_price = serializers.SerializerMethodField()
+    min_delivery_time = serializers.SerializerMethodField()
+    user_details = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Offer
+        fields = ['id', 'user', 'title', 'image', 'description', 'created_at', 'updated_at', 'details', 'min_price', 'min_delivery_time', 'user_details']
         extra_kwargs = {
             'user': {'read_only': True}
         }
@@ -20,33 +64,45 @@ class OfferSerializer(serializers.ModelSerializer):
             "username": obj.user.username
         }
 
-    def create(self, validated_data):
-        user = self.context['request'].user  # Holt den aktuell eingeloggten Benutzer
-        validated_data['user'] = user
+    def get_details(self, obj):
+        request = self.context.get('request')
+        return [
+            {
+                "id": detail.id,
+                "url": request.build_absolute_uri(reverse("offerdetails-detail", args=[detail.id]))
+            }
+            for detail in obj.details.all()
+        ]
 
-        details_data = validated_data.pop('details')  # Holt die Details-Daten
-        offer = Offer.objects.create(**validated_data)  # Erstellt das Angebot
-        for detail in details_data:
-            # Erstellt für jedes Detail einen OfferDetail-Eintrag
-            OfferDetail.objects.create(offer=offer, **detail)
-        # return offer
+    def get_min_price(self, obj):
+        return obj.details.aggregate(Min("price"))["price__min"]
 
-        return super().create(validated_data)
-
-    def update(self, instance, validated_data):
-        details_data = validated_data.pop('details')  # Holt die Details-Daten
-        instance = super().update(instance, validated_data)
-        
-        # Löscht alte OfferDetail und erstellt sie erneut
-        instance.details.all().delete()
-        for detail in details_data:
-            OfferDetail.objects.create(offer=instance, **detail)
-        
-        return instance
+    def get_min_delivery_time(self, obj):
+        return obj.details.aggregate(Min("delivery_time_in_days"))["delivery_time_in_days__min"]
 
 
-class OfferDetailSerializer(serializers.ModelSerializer):
+class OfferDetailViewSerializer(serializers.ModelSerializer):   # GET Retrieve
+    details = serializers.SerializerMethodField()
+    min_price = serializers.SerializerMethodField()
+    min_delivery_time = serializers.SerializerMethodField()
 
     class Meta:
-        model = OfferDetail
-        fields = ['id', 'title', 'revisions', 'delivery_time_in_days', 'price', 'features', 'offer_type']
+        model = Offer
+        fields = ['id', 'user', 'title', 'image', 'description', 'created_at', 'updated_at', 'details', 'min_price', 'min_delivery_time']
+
+    def get_details(self, obj):
+        request = self.context.get('request')
+        return [
+            {
+                "id": detail.id,
+                "url": request.build_absolute_uri(reverse("offerdetails-detail", args=[detail.id]))
+            }
+            for detail in obj.details.all()
+        ]
+
+    def get_min_price(self, obj):
+        return obj.details.aggregate(Min("price"))["price__min"]
+
+    def get_min_delivery_time(self, obj):
+        return obj.details.aggregate(Min("delivery_time_in_days"))["delivery_time_in_days__min"]
+
