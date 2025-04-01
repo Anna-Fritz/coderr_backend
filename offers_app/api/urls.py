709b7a4f415131ae32cfaeb53eb2e7d7ce5e37b1
Anna Fritz <anna_eida@gmx.de>
2025-04-01 15:29:46 +0200

    feat(view): optimize OfferViewSet POST/PATCH requests

--- offers_app/api/urls.py
+++ offers_app/api/urls.py
@@ -9,6 +9,4 @@ router.register(r'offers', OfferViewSet, basename='offer')
 urlpatterns = [
     path('', include(router.urls)),
     path('offerdetails/<int:pk>/', OfferDetailDetailView.as_view(), name='offerdetails-detail'),
-    # path('offers/', OffersListView.as_view(), name='offers-list'),
-    # path('offers/<int:pk>/', OfferDetailView.as_view(), name='offer-detail'),
 ]
--- offers_app/api/views.py
+++ offers_app/api/views.py
@@ -1,39 +1,60 @@
 from rest_framework import viewsets, generics
 from ..models import Offer, OfferDetail
-from .serializers import OfferSerializer, OfferDetailSerializer, OfferListSerializer, OfferDetailViewSerializer
+from .serializers import OfferUpdateSerializer, OfferDetailSerializer, OfferListSerializer, OfferDetailViewSerializer, OfferCreateSerializer
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework import filters
+from django_filters.rest_framework import DjangoFilterBackend
+from .pagination import LargeResultsSetPagination
+from .filters import OfferFilter
+from django.db.models import Min, Max
 
 
 class OfferViewSet(viewsets.ModelViewSet):
+    filterset_class = OfferFilter
+    search_fields = ['title', 'description']
+    ordering_fields = ['updated_at', 'min_price']
+    ordering = ['updated_at']
+    pagination_class = LargeResultsSetPagination
     queryset = Offer.objects.all()
-    # serializer_class = OfferSerializer
+    serializer_class = OfferListSerializer
+
+    def get_queryset(self):
+        """
+        Returns the filtered and annotated queryset for listing objects.
+        Annotates the queryset with the minimum price and maximum delivery time.
+        Applies filtering, searching, and ordering only for GET requests in the 'list' action.
+        """
+        queryset = super().get_queryset().annotate(min_price=Min("details__price"), max_delivery_time=Max("details__delivery_time_in_days"))
+        if self.request.method == "GET" and self.action == 'list':
+            filter_backends = [DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter]
+            for backend in filter_backends:
+                queryset = backend().filter_queryset(self.request, queryset, self)
+        return queryset
 
     def get_serializer_class(self):
         if self.action == "list":
             return OfferListSerializer  # GET /offers/
         if self.action == "retrieve":
             return OfferDetailViewSerializer  # GET /offers/{id}/
-        return OfferSerializer  # POST, PUT, PATCH
-
-    # def perform_create(self, serializer):
-    #     serializer.save(user=self.request.user)
+        if self.action == 'create':
+            return OfferCreateSerializer  # POST /offers/
+        return OfferUpdateSerializer  # PUT, PATCH  /offers/{id}/
 
     def create(self, request, *args, **kwargs):
         """
-        Erstellt ein neues Offer und gibt eine benutzerdefinierte Response zurück.
+        Creates a new offer associated with the authenticated user.
+        Validates request data, saves the offer, and returns the serialized response.
         """
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
-        offer = serializer.save(user=request.user)
-
-        # Falls du eine detailliertere Response zurückgeben möchtest
-        response_serializer = OfferSerializer(offer, context={"request": request})
+        offer = serializer.save(user_id=request.user.id)
+        response_serializer = OfferCreateSerializer(offer, context={"request": request})
         return Response(response_serializer.data, status=status.HTTP_201_CREATED)
 
     def update(self, request, *args, **kwargs):
-        partial = kwargs.pop('partial', False)  # Prüft, ob es ein PATCH ist
+        partial = kwargs.pop('partial', False)  # check if request is PATCH
         instance = self.get_object()
         serializer = self.get_serializer(instance, data=request.data, partial=partial)
         serializer.is_valid(raise_exception=True)
