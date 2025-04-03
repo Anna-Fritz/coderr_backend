0c9bd4e8be9fc3e892d43285cd4ab1f21184f3a7
Anna Fritz <anna_eida@gmx.de>
2025-04-03 23:57:32 +0200

    feat(view): create OrderViewSet

--- orders_app/api/views.py
+++ orders_app/api/views.py
@@ -1,5 +1,12 @@
 from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
+
+from ..models import Order
+from .serializers import OrderSerializer
+from .permissions import IsCustomerOrAdminOrReadOnly
 
 
 class OrderViewSet(viewsets.ModelViewSet):
-    pass
+    queryset = Order.objects.all()
+    serializer_class = OrderSerializer
+    permission_classes = [IsAuthenticated, IsCustomerOrAdminOrReadOnly]
