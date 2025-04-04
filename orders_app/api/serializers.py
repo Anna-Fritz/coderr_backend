b86c71f5a295e459ceaa4c51bfde2dbe12e5e6d8
Anna Fritz <anna_eida@gmx.de>
2025-04-04 12:53:51 +0200

    feat(serializer): optimize create() and create update() methods of OrderSerializer

--- orders_app/api/serializers.py
+++ orders_app/api/serializers.py
@@ -1,44 +1,45 @@
 from rest_framework import serializers
+from django.shortcuts import get_object_or_404
 
 from ..models import Order
 from offers_app.models import OfferDetail
-from profiles_app.models import UserProfile
+from user_auth_app.models import CustomUser
 
 
 class OrderSerializer(serializers.ModelSerializer):
-    offer_detail_id = serializers.IntegerField(write_only=True)
+    offer_detail_id = serializers.PrimaryKeyRelatedField(queryset=OfferDetail.objects.all(), write_only=True)
 
     class Meta:
         model = Order
         fields = ['id', 'customer_user', 'business_user', 'title', 'revisions', 'delivery_time_in_days', 'price', 'features', 'offer_type', 'status', 'created_at', 'updated_at', 'offer_detail_id']
-        read_only_fields = ['id', 'customer_user', 'business_user', 'title', 'revisions', 'delivery_time_in_days', 'price', 'features', 'offer_type', 'status', 'created_at', 'updated_at']
+        read_only_fields = ['id', 'customer_user', 'business_user', 'title', 'revisions', 'delivery_time_in_days', 'price', 'features', 'offer_type', 'created_at', 'updated_at']
 
     def create(self, validated_data):
-        user = self.context['request'].user
-        customer_profile = UserProfile.objects.get(user_id=user.id)
-        if not user or not user.is_authenticated:
+        request_user = self.context['request'].user
+        if not request_user.is_authenticated:
             raise serializers.ValidationError({"user": "Authentication is required."})
-        if not customer_profile:
-            raise serializers.ValidationError({"user": "UserProfile does not exist"})
 
-        offer_detail_id = validated_data.pop('offer_detail_id', None)
-        try:
-            offer_detail = OfferDetail.objects.get(id=offer_detail_id)
-        except OfferDetail.DoesNotExist:
-            raise serializers.ValidationError({"offer_detail_id": "OfferDetail with this ID does not exist."})
-        business_user = offer_detail.offer.user
-        business_user_profile = UserProfile.objects.get(user_id=business_user.id)
+        customer_user = get_object_or_404(CustomUser, id=request_user.id)
+        offer_detail_id = validated_data.pop('offer_detail_id')
+        business_user = get_object_or_404(CustomUser, id=offer_detail_id.offer.user.id)
 
         order = Order.objects.create(
-            customer_user=customer_profile,
-            business_user=business_user_profile,
-            title=offer_detail.title,
-            revisions=offer_detail.revisions,
-            delivery_time_in_days=offer_detail.delivery_time_in_days,
-            price=offer_detail.price,
-            features=offer_detail.features,
-            offer_type=offer_detail.offer_type,
+            customer_user=customer_user,
+            business_user=business_user,
+            title=offer_detail_id.title,
+            revisions=offer_detail_id.revisions,
+            delivery_time_in_days=offer_detail_id.delivery_time_in_days,
+            price=offer_detail_id.price,
+            features=offer_detail_id.features,
+            offer_type=offer_detail_id.offer_type,
             status="in_progress"
         )
-
         return order
+
+    def update(self, instance, validated_data):
+        status = validated_data.pop('status', None)
+        instance = super().update(instance, validated_data)
+        if status:
+            instance.status = status
+            instance.save()
+        return instance
