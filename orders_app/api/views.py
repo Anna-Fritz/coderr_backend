8d6830cdfb186ab53e11d5e3ac71588e8a6eb06b
Anna Fritz <anna_eida@gmx.de>
2025-04-04 12:54:17 +0200

    feat(view): create update() for OrderViewSet

--- orders_app/api/views.py
+++ orders_app/api/views.py
@@ -1,5 +1,6 @@
 from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 
 from ..models import Order
 from .serializers import OrderSerializer
@@ -10,3 +11,11 @@ class OrderViewSet(viewsets.ModelViewSet):
     queryset = Order.objects.all()
     serializer_class = OrderSerializer
     permission_classes = [IsAuthenticated, IsCustomerOrAdminOrReadOnly]
+
+    def update(self, request, *args, **kwargs):
+        partial = kwargs.pop('partial', False)  # check if request is PATCH
+        instance = self.get_object()
+        serializer = self.get_serializer(instance, data=request.data, partial=partial)
+        serializer.is_valid(raise_exception=True)
+        self.perform_update(serializer)
+        return Response(serializer.data)
