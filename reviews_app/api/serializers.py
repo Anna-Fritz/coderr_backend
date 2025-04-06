0e89eaee0f028d3c85f4fc12297caef91bccc954
Anna Fritz <anna_eida@gmx.de>
2025-04-06 23:54:06 +0200

    feat(serializer): create ReviewSerializer

---
+++ reviews_app/api/serializers.py
@@ -0,0 +1,40 @@
+from rest_framework import serializers
+from django.shortcuts import get_object_or_404
+
+from ..models import Review
+from user_auth_app.models import CustomUser
+
+
+class ReviewSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Review
+        fields = ['id', 'business_user', 'reviewer', 'rating', 'description', 'created_at', 'updated_at']
+        read_only_fields = ['id', 'reviewer']
+
+    def validate(self, data):
+        user = self.context['request'].user
+        business_user = data.get('business_user')
+        if Review.objects.filter(business_user=business_user, reviewer=user).exists():
+            raise serializers.ValidationError("You have already reviewed this business user.")
+        return data
+
+    def create(self, validated_data):
+        request_user = self.context['request'].user
+        business_user = validated_data.pop('business_user')
+        rating = validated_data.pop('rating')
+        description = validated_data.pop('description')
+
+        review = Review.objects.create(
+            business_user=business_user,
+            reviewer=request_user,
+            rating=rating,
+            description=description,
+        )
+        return review
+
+    def update(self, instance, validated_data):
+        instance.rating = validated_data.get('rating', instance.rating)
+        instance.description = validated_data.get('description', instance.description)
+        instance.save()
+
+        return instance
