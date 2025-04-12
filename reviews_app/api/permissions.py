8c9efcab881ac8194a4dbd1083f2d01f1cc9795d
Anna Fritz <anna_eida@gmx.de>
2025-04-12 23:59:19 +0200

    feat(permission): optimize permission for ReviewViewSet

--- reviews_app/api/permissions.py
+++ reviews_app/api/permissions.py
@@ -2,10 +2,15 @@ from rest_framework.permissions import BasePermission, SAFE_METHODS
 
 
 class IsReviewerOrBusinessUserOrAdmin(BasePermission):
+    def has_permission(self, request, view):
+        if request.method == "POST":
+            return request.user.is_authenticated and request.user.type == "customer"
+        return True
+
     def has_object_permission(self, request, view, obj):
         if request.method in SAFE_METHODS:
             return request.user == obj.reviewer or request.user == obj.business_user
-        if request.method in ["POST", "PATCH", "PUT"]:
+        if request.method in ["PATCH", "PUT"]:
             return request.user == obj.reviewer
         if request.method == "DELETE":
             return request.user == obj.reviewer or request.user.is_superuser
